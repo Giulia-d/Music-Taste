79864d16342b9afc2812d009306d038dd0eef611
Stella Cervini <s.cervini1@campus.unimib.it>
2021-05-19 16:51:04 +0200

    Added lyrics to SongActivity

--- MusicTaste/app/src/main/java/it/unimib/musictaste/SongActivity.java
+++ MusicTaste/app/src/main/java/it/unimib/musictaste/SongActivity.java
@@ -8,11 +8,24 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import it.unimib.musictaste.fragments.SearchFragment;
 import it.unimib.musictaste.utils.GradientTransformation;
 import it.unimib.musictaste.utils.Song;
+
 
 public class SongActivity extends AppCompatActivity {
 
@@ -38,8 +51,53 @@ public class SongActivity extends AppCompatActivity {
         Picasso.get().load(song.getImage()).transform(new GradientTransformation()).into(imgSong);
         tvArtistSong.setText(song.getArtist());
         tvTitleSong.setText(song.getTitle());
-        tvLyricsSong.setText(song.getId());
+        //tvLyricsSong.setText(song.getId());
         //Log.d("user", "Photo:" + tre);
+        getLyrics(song);
     }
+
+    public void getLyrics(Song song){
+        /*RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.lyrics.ovh/v1/" + song.getArtist() + "/" + song.getTitle();
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        tvLyricsSong.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tvLyricsSong.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);*/
+        String url ="https://api.lyrics.ovh/v1/" + song.getArtist() + "/" + song.getTitle();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url , null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response){
+                try {
+                    Log.d("lyrics", response.toString());
+                    tvLyricsSong.setText(response.getString("lyrics"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("tag", "onErrorResponse: " + error.getMessage());
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
+
 
 }
