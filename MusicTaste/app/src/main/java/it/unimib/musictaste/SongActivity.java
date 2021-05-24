8b9d0222616b275c17fd00b3932cfa67167c42eb
Nicolas Calmi <nicolas.calmi@dilium.com>
2021-05-24 17:52:05 +0200

    Parsed description and showed in textview. Parsed youtube and spotify url and created image button for youtube spotify and favorite songs

--- MusicTaste/app/src/main/java/it/unimib/musictaste/SongActivity.java
+++ MusicTaste/app/src/main/java/it/unimib/musictaste/SongActivity.java
@@ -2,12 +2,19 @@ package it.unimib.musictaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.EventLogTags;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,9 +29,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import it.unimib.musictaste.fragments.SearchFragment;
 import it.unimib.musictaste.utils.GradientTransformation;
 import it.unimib.musictaste.utils.Song;
+import it.unimib.musictaste.utils.Utils;
 
 
 public class SongActivity extends AppCompatActivity {
@@ -33,6 +44,9 @@ public class SongActivity extends AppCompatActivity {
     TextView tvArtistSong;
     TextView tvTitleSong;
     TextView tvLyricsSong;
+    TextView tvDescription;
+    ImageButton mbtnYt, mbtnSpotify, mbtnLike;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +56,11 @@ public class SongActivity extends AppCompatActivity {
         imgSong = findViewById(R.id.imgSong);
         tvArtistSong = findViewById(R.id.tvArtistSong);
         tvTitleSong = findViewById(R.id.tvTitleSong);
-        tvLyricsSong = findViewById(R.id.tvLyricsSong);
+        //tvLyricsSong = findViewById(R.id.tvLyricsSong);
+        tvDescription = findViewById(R.id.tvDescription);
+        mbtnYt = findViewById(R.id.btnYoutube);
+        mbtnSpotify = findViewById(R.id.btnSpotify);
+        mbtnLike = findViewById(R.id.btnLike);
 
         Intent intent = getIntent();
 
@@ -53,38 +71,116 @@ public class SongActivity extends AppCompatActivity {
         tvTitleSong.setText(song.getTitle());
         //tvLyricsSong.setText(song.getId());
         //Log.d("user", "Photo:" + tre);
-        getLyrics(song);
-    }
 
-    public void getLyrics(Song song){
-        /*RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://api.lyrics.ovh/v1/" + song.getArtist() + "/" + song.getTitle();
+        getDescription(song);
 
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        tvLyricsSong.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
+        mbtnYt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                tvLyricsSong.setText("That didn't work!");
+            public void onClick(View v) {
+                if(song.getYoutube() != null){
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(song.getYoutube()));
+                    try {
+                        SongActivity.this.startActivity(webIntent);
+                    } catch (ActivityNotFoundException ex) {
+                    }
+                }
+                else
+                    Toast.makeText(SongActivity.this, R.string.YoutubeError, Toast.LENGTH_LONG).show();
             }
         });
 
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);*/
-        String url ="https://api.lyrics.ovh/v1/" + song.getArtist() + "/" + song.getTitle();
-        RequestQueue queue = Volley.newRequestQueue(this);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url , null, new Response.Listener<JSONObject>() {
+        mbtnSpotify.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(JSONObject response){
+            public void onClick(View v) {
+                if(song.getSpotify() != null){
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(song.getSpotify()));
+                    try {
+                        SongActivity.this.startActivity(webIntent);
+                    } catch (ActivityNotFoundException ex) {
+                    }
+                }
+                else
+                    Toast.makeText(SongActivity.this, R.string.SpotifyError, Toast.LENGTH_LONG).show();
+            }
+        });
+        //getLyrics(song);
+    }
+
+    public String digger(JSONArray children) throws JSONException {
+        String description = "";
+        for (int j = 0; j < children.length(); j++) {
+            if (children.get(j) instanceof String)
+                description = description + children.get(j);
+            else if (children.getJSONObject(j).has("children"))
+                description = description + digger(children.getJSONObject(j).getJSONArray("children"));
+        }
+        return description;
+    }
+
+    public void getDescription(Song song) {
+        String url = "https://api.genius.com/songs/" + song.getId();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject responseDescription = response.getJSONObject("response").getJSONObject("song").getJSONObject("description").getJSONObject("dom");
+                    JSONArray desc = responseDescription.getJSONArray("children");
+                    String description = "";
+                    for (int i = 0; i < desc.length(); i++) {
+                        if (!(desc.get(i) instanceof String)) {
+                            JSONArray children = desc.getJSONObject(i).getJSONArray("children");
+                            description = description + digger(children);
+                        }
+                    }
+                    if (description.equals("?"))
+                        description = getString(R.string.Description);
+                    tvDescription.setText(description);
+
+                    //Find youtube and spotify links from response
+                    JSONArray media = response.getJSONObject("response").getJSONObject("song").getJSONArray("media");
+                    Log.d("media", media.toString());
+                    if (media != null) {
+                        if (media.getJSONObject(0).has("url"))
+                            song.setYoutube(media.getJSONObject(0).getString("url"));
+                        if (media.length() > 1) {
+                            if (media.getJSONObject(1).has("url"))
+                                song.setSpotify(media.getJSONObject(1).getString("url"));
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("tag", "onErrorResponse: " + error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                //params.put("Content-Type", "application/json; charset=UTF-8");
+                params.put("Authorization", "Bearer " + Utils.ACCESS_TOKEN);
+                return params;
+            }
+        };
+        queue.add(jsonObjectRequest);
+    }
+
+    public void getLyrics(Song song) {
+        String url = "https://api.lyrics.ovh/v1/" + song.getArtist() + "/" + song.getTitle();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
                 try {
                     Log.d("lyrics", response.toString());
-                    tvLyricsSong.setText(response.getString("lyrics"));
+                    tvDescription.setText(response.getString("lyrics"));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
--- MusicTaste/app/src/main/java/it/unimib/musictaste/utils/Song.java
+++ MusicTaste/app/src/main/java/it/unimib/musictaste/utils/Song.java
@@ -8,6 +8,8 @@ public class Song implements Parcelable{
     private String artist;
     private String image;
     private String id;
+    private String youtube;
+    private String spotify;
 
     public Song(String title, String image, String id, String artist) {
         this.title = title;
@@ -47,6 +49,21 @@ public class Song implements Parcelable{
         this.id = id;
     }
 
+    public String getYoutube() {
+        return youtube;
+    }
+
+    public void setYoutube(String youtube) {
+        this.youtube = youtube;
+    }
+
+    public void setSpotify(String spotify) {
+        this.spotify = spotify;
+    }
+
+    public String getSpotify() {
+        return spotify;
+    }
 
     @Override
     public int describeContents() {
